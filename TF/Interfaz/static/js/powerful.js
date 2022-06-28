76300360636d23a39f8751d3b1014a749b127a8e
educaba123 <u202019644@upc.edu.pe>
2022-06-28 00:27:22 -0500

    a

--- TF/Interfaz/static/js/powerful.js
+++ TF/Interfaz/static/js/powerful.js
@@ -1,62 +1,104 @@
-(async function() {
-	console.log("Toda la alegría del mundo.");
+(async function () {
+  console.log("Toda la alegría del mundo.");
 
-	// Data
+  // Data
+  const urlgraph = "graph";
+  const graph = await d3.json(urlgraph);
 
-	const urlgraph = "graph";
-	const urlpaths = "paths"
-	const graph    = await d3.json(urlgraph);
-	const paths    = await d3.json(urlpaths);
+  const s = Math.floor(Math.random() * graph.g.length);
+  const t = Math.floor(Math.random() * graph.g.length);
+  const urlpaths = `paths/${s}/${t}`
+  const paths = await d3.json(urlpaths);
 
-	// config
+  // config
 
-	const margin = {
-		top:    10,
-		right:  10,
-		bottom: 10,
-		left:   10
-	};
-	const box    = {
-		width:   990,
-		height:  695,
-		bwidth:  990 - margin.left - margin.right,
-	  bheight: 695 - margin.top - margin.bottom
-	};
+  const margin = {
+    top: 10,
+    right: 10,
+    bottom: 10,
+    left: 10
+  };
+  const box = {
+    width: 1600,
+    height: 1000,
+    bwidth: 1600 - margin.left - margin.right,
+    bheight: 1000 - margin.top - margin.bottom,
+  };
 
-	// Canvas y elementos
+  // Canvas y elementos
 
-	const svg = d3
-		.select("#box")
-		.append("svg")
-		.attr("width", box.width)
-		.attr("height", box.height);
+  const ctx = document.querySelector("#canvitas").getContext("2d");
+  if (!ctx) {
+    console.log("something terribly wrong is going on here");
+    return;
+  }
+  ctx.canvas.width = box.width;
+  ctx.canvas.height = box.height;
 
-	const g = svg.append("g")
-		.attr("transform", `translate(${margin.left}, ${margin.top})`);
+  const extentx = d3.extent(graph.loc, d => d[0]);
+  const extenty = d3.extent(graph.loc, d => d[1]);
+  const w = extentx[1] - extentx[0];
+  const h = extenty[1] - extenty[0];
+  
+  let size = 0, xpro = 1, ypro = 1;
+  size = (w > h) ? (box.bwidth - margin.right) : (box.bheight - margin.bottom);
+  xpro = (w > h) ? 1 : (w / h);
+  ypro = (w > h) ? (h / w) : 1;
 
-	const [lon, lat] = [d => d[0], d => d[1]];
-	const lineGenerator = d3.line().x(lon).y(lat);
-	const line = g.append("path")
-		.attr("d", lineGenerator(graph.loc))
-		.attr("fill", "none")
-		.attr("stroke", "Gold")
-		.attr("stroke-width", 1.5)
-		.attr("opacity", 0.75);
-	const dots = g.selectAll("circle")
-		.data(graph.loc)
-		.enter()
-		.append("circle")
-		.attr("cx", lon)
-		.attr("cy", lat)
-		.attr("r", 2.2)
-		.attr("fill", "Orange")
-		.attr("opacity", 0.75);
+  scalex = d3.scaleLinear()
+    .domain(extentx)
+    .range([margin.left, size * xpro]);
+  scaley = d3.scaleLinear()
+    .domain(extenty)
+    .range([size * ypro, margin.top]);
 
-	// Funciones y eventos
+  const [lon, lat] = [d => scalex(d[0]), d => scaley(d[1])];
+  const x = d => lon(d);
+  const y = d => lat(d);
 
-	// Empezamos
+  function render(points, color, lw) {
+    ctx.lineWidth = lw;
+    ctx.beginPath();
+    ctx.strokeStyle = color;
+    for (const point of points) {
+      ctx.moveTo(x(point[0]), y(point[0]));
+      ctx.lineTo(x(point[1]), y(point[1]));
+    }
+    ctx.stroke();
+  }
+
+  const edges = [];
+  for (const u in graph.g) {
+    for (const [v, _] of graph.g[u]) {
+      edges.push([graph.loc[u], graph.loc[v]])
+    }
+  }
+  render(edges, 'white', 2)
+  
+  function dealWithPath(path, color) {
+    let head = t;
+    points = []
+    while (path[head] != -1) {
+      points.push([graph.loc[head], graph.loc[path[head]]]);
+      head = path[head];
+    }
+    render(points, color, 4)
+  }
+  dealWithPath(paths.bestpath, "darkgreen")
+  dealWithPath(paths.path1, "orange")
+  dealWithPath(paths.path2, "red")
+
+  ctx.fillStyle = "LimeGreen";
+  ctx.fillRect(x(graph.loc[s]) - 5, y(graph.loc[s]) - 5, 10, 10)
+  ctx.strokeStyle = "Green";
+  ctx.strokeRect(x(graph.loc[s]) - 5, y(graph.loc[s]) - 5, 10, 10)
+  ctx.fillStyle = "Orange";
+  ctx.fillRect(x(graph.loc[t]) - 5, y(graph.loc[t]) - 5, 10, 10)
+  ctx.strokeStyle = "OrangeRed";
+  ctx.strokeRect(x(graph.loc[t]) - 5, y(graph.loc[t]) - 5, 10, 10)
+
+  // Funciones y eventos
+
+  // Empezamos
 
 })();
-
-/* vim: set tabstop=2:softtabstop=2:shiftwidth=2:noexpandtab */
-
