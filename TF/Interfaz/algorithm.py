11da0bc9b2abf6afa4e7f8203fcc20b649718fd7
educaba123 <u202019644@upc.edu.pe>
2022-06-30 01:20:50 -0500

    aaa

--- TF/Interfaz/algorithm.py
+++ TF/Interfaz/algorithm.py
@@ -1,4 +1,110 @@
 import json
+<<<<<<< Updated upstream
+=======
+import random as r
+import math
+import heapq as hq
+import re
+
+
+def pasarLoc():
+  with open("loc.txt",) as f:
+    G = []
+    a = []
+    b = 0
+    for line in f:
+      if line[1].isdigit():
+        b = b+1
+        line = re.sub("\,|\\n", "", line)
+        nums = [float(x) for x in line.split()]
+        for i in range(0, len(nums), 2):
+          G.append((nums[i], nums[i+1]))
+      else:
+        a.append(b)
+        b = 0
+  a.pop(0)
+  return G, a
+
+def transformGraph():
+    #n, m = 8, 56
+    #Loc = [(i * 100 - r.randint(145, 155), j * 100 - r.randint(145, 155))
+    #       for i in range(1, n + 1) for j in range(1, m + 1)]
+    Loc, x =pasarLoc()
+    suma = 0
+    for fila in x:
+      suma += fila
+    n=len(x)
+    G = [[] for _ in range(suma +1-19)]
+    for i in range(n):
+        m=x[i]
+        for j in range(m):
+            adjs = [(i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1)]
+            for u, v in adjs:
+                if u >= 0 and u < n and v >= 0 and v < m:
+                    G[i * m + j].append((u * m + v, r.randint(1, 345353)))
+    return G, Loc
+
+
+def bfs(G, s):
+  n = len(G)
+  visited = [False]*n
+  path = [-1]*n  # parent
+  queue = [s]
+  visited[s] = True
+
+  while queue:
+    u = queue.pop(0)
+    for v, _ in G[u]:
+      if not visited[v]:
+        visited[v] = True
+        path[v] = u
+        queue.append(v)
+
+  return path
+
+
+def dfs(G, s):
+  n = len(G)
+  path = [-1]*n
+  visited = [False]*n
+
+  def _dfs(u):
+    visited[u] = True
+    for v, _ in G[u]:
+      if not visited[v]:
+        path[v] = u
+        _dfs(v)
+
+  _dfs(s)
+  return path
+
+
+def dijkstra(G, s):
+    n = len(G)
+    visited = [False]*n
+    path = [-1]*n
+    cost = [math.inf]*n
+
+    cost[s] = 0
+    pqueue = [(0, s)]
+    while pqueue:
+        g, u = hq.heappop(pqueue)
+        if not visited[u]:
+            visited[u] = True
+            for v, w in G[u]:
+                if not visited[v]:
+                    f = g + w
+                    if f < cost[v]:
+                        cost[v] = f
+                        path[v] = u
+                        hq.heappush(pqueue, (f, v))
+
+    return path, cost
+
+
+G, Loc = transformGraph()
+
+>>>>>>> Stashed changes
 
 def graph():
     Loc = [(10, 10), (10, 24), (23, 22), (20, 11)]
